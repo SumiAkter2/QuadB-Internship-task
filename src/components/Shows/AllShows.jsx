ecf9f772876ad2e73fe820e1c28218475b23b8a8
SumiAkter2 <akteralisha36@gmail.com>
2023-05-06 18:36:31 +0600

    added style in allshows

--- src/components/Shows/AllShows.jsx
+++ src/components/Shows/AllShows.jsx
@@ -1,10 +1,16 @@
-const AllShows = (  ) => {
-    // const {show}=allShow
+import'./Shows.css';
+const AllShows = (props) => {
+    const { show,  } = props.allShow;
+   
     return (
         <div>
-            <p>
-               <h1>hello</h1>
-        </p>
+            <div className="shows-content ">
+                {/* <h1>{show.id}</h1> */}
+                <img src={show.image?.original} alt="" />
+                <h3>{show?.name}</h3>
+                <p>Genres: {show?.genres}</p>
+                {/* <p>{show?.schedule?.summary}</p> */}
+        </div>
         </div>
     );
 };
--- src/components/Shows/Shows.jsx
+++ src/components/Shows/Shows.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import AllShows from "./AllShows";
-
+import './shows.css';
 const Shows = () => {
     const [shows, setShows] = useState([]);
     const url = 'https://api.tvmaze.com/search/shows?q=all';
     useEffect(() => {
         fetch(url).then(res=>res.json()).then(data=>setShows(data))
-    },[])
+    }, [])
+    console.log(shows);
     return (
         <div>
-            <h2>Our Popular Shows Here</h2>
-            {shows.map(allShow=><AllShows key={allShow.show?.id} allShow={allShow}></AllShows>)}
+            <h2 className="ms-2 py-4">Our Popular Shows Here</h2>
+            <div>
+                <div className="shows-container" >
+                    {shows.map(allShow=><AllShows key={allShow.show?.id} allShow={allShow}></AllShows>)}
+                  </div>
+          </div>
         </div>
     );
 };
